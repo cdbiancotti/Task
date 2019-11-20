d06fd55a690b05b1ea886cc10b19b64ed5f4ee5b
Cistian Daniel Biancotti <cdbiancotti@gmail.com>
2019-11-20 02:35:01 -0300

    First step in the task app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import tasks from './sample/tasks.json';
 
-function App() {
-  return (
-    <div>
-      Hello World!
+class App extends Component{
+
+  state = {
+    tasks: tasks,
+  }
+
+  render () {
+    return <div>
+      <h1>Hello World!</h1>
+      { this.state.tasks.map( e => <h2> {e.title} </h2>) }
     </div>
-  );
+  }
 }
 
 export default App;
